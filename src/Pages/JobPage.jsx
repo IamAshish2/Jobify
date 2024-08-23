54a8fde1a817224a77cee82ee4f4eeaabcc1610c
IamAshish2 <karkiaashish9899@gmail.com>
2024-08-23 12:46:37 +0545

    added respective job page for all jobs

--- src/Pages/JobPage.jsx
+++ src/Pages/JobPage.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../Components/Spinner";
 
 const JobPage = () => {
-    const {id} = useParams();
-  return (
-    <div>
-      Hello 
-    </div>
-  )
-}
+  const { id } = useParams();
+  const [job, setJob] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default JobPage
+  useEffect(() => {
+    try {
+      const fetchJob = async () => {
+        const res = await axios.get(`http://localhost:8000/jobsData/${id}`);
+        console.log(res.data);
+        setJob(res.data);
+      };
+      fetchJob();
+    } catch (er) {
+      console.log("error fetching data", err);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  loading ? <Spinner /> : "";
+
+  return <div>Hello</div>;
+};
+
+export default JobPage;
