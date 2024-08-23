5a6fa85dc7d2ccff8355d44c7b09e821c17f4e49
IamAshish2 <karkiaashish9899@gmail.com>
2024-08-23 21:28:39 +0545

    login page ui

---
+++ src/Pages/auth/Login.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import loginPhoto from '../../assets/background.jpg';
+
+const Login = () => {
+  return (
+    <div className="min-h-screen flex">
+      {/* Form Section */}
+      <div className="w-full lg:w-1/2 flex items-center justify-center bg-gray-50 p-8">
+        <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
+          <h2 className="text-4xl font-bold text-center mb-10 text-gray-800">Login</h2>
+          <form className="space-y-8">
+            <div>
+              <input
+                className="w-full p-5 border border-blue-400 rounded-lg focus:outline-none focus:border-blue-500"
+                type="email"
+                placeholder="Enter Your Email"
+              />
+            </div>
+            <div>
+              <input
+                className="w-full p-5 border border-blue-400 rounded-lg focus:outline-none focus:border-blue-500"
+                type="password"
+                placeholder="Enter Your Password"
+              />
+            </div>
+            <button
+              className="w-full p-5 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-600 transition duration-300"
+              type="submit"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+
+      {/* Image Section */}
+      <div
+        className="hidden  lg:flex lg:w-1/2 bg-cover bg-right"
+        style={{
+          backgroundImage: `url(${loginPhoto})`,
+          backgroundPosition:top,
+
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default Login;
