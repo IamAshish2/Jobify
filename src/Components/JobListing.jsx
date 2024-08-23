4216f801cab5a150034a04ca306fb2309e337dab
IamAshish2 <karkiaashish9899@gmail.com>
2024-08-23 18:57:48 +0545

    CleanUp

--- src/Components/JobListing.jsx
+++ src/Components/JobListing.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import {FaMapMarker} from 'react-icons/fa'
+import { FaMapMarker } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const JobListing = ({ job }) => {
   const [showDescription, setShowFullDescription] = useState(false);
   let description = job.description;
+
   if (!showDescription) description = description.substring(0, 90) + "...";
 
   return (
@@ -18,7 +20,7 @@ const JobListing = ({ job }) => {
           {description}
           <p
             onClick={() => {
-              setShowFullDescription((prevState) => !prevState );
+              setShowFullDescription((prevState) => !prevState);
             }}
             className={`text-blue-600 mb-5 hover:cursor-pointer`}
           >
@@ -32,15 +34,15 @@ const JobListing = ({ job }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <FaMapMarker className="inline text-lg mb-1 mr-2 "/>
+            <FaMapMarker className="inline text-lg mb-1 mr-2 " />
             {job.location}
           </div>
-          <a
-            href={`jobs/${job.id}`}
+          <Link
+            to={`/jobs/${job.id}`}
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
-          </a>
+          </Link>
         </div>
       </div>
     </div>
