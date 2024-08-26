848359788cb30472599d2ce9da0d7719ceb01e21
IamAshish2 <karkiaashish9899@gmail.com>
2024-08-26 21:20:37 +0545

    signup and login pages

--- src/Pages/auth/Login.jsx
+++ src/Pages/auth/Login.jsx
@@ -1,28 +1,71 @@
-import React from 'react';
-import loginPhoto from '../../assets/signin.png';
-import { Link } from 'react-router-dom';
+import React from "react";
+import loginPhoto from "../../assets/signin.png";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { signInSchema } from "../../validation/formValidation";
+import axios from "axios";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(signInSchema),
+  });
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5079/api/Login",
+        data
+      );
+      console.log(response.data);
+      if(response.status === 200){
+        const {token,userId,role} = response.data;
+        localStorage.setItem("token",token);
+        localStorage.setItem("role",role);
+        localStorage.setItem("userId",userId);
+        navigate("/");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="min-h-screen flex">
       {/* Form Section */}
       <div className="w-full lg:w-1/2 flex items-center justify-center bg-gray-50 p-8">
         <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
-          <h2 className="text-4xl font-bold text-center mb-10 text-gray-800">Login</h2>
-          <form className="space-y-8">
+          <h2 className="text-4xl font-bold text-center mb-10 text-gray-800">
+            Login
+          </h2>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
             <div>
               <input
                 className="w-full p-5 border border-blue-400 rounded-lg focus:outline-none focus:border-blue-500"
-                type="email"
-                placeholder="Enter Your Email"
+                name="UserName"
+                {...register("UserName")}
+                placeholder="Enter Your userName"
               />
+              {errors.UserName && (
+                <p className="text-red-600">{errors.UserName.message}</p>
+              )}
             </div>
             <div>
               <input
                 className="w-full p-5 border border-blue-400 rounded-lg focus:outline-none focus:border-blue-500"
                 type="password"
+                name="Password"
+                {...register("Password")}
                 placeholder="Enter Your Password"
               />
+              {errors.Password && (
+                <p className="text-red-600">{errors.Password.message}</p>
+              )}
             </div>
             <button
               className="w-full p-5 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-600 transition duration-300"
@@ -30,7 +73,15 @@ const Login = () => {
             >
               Login
             </button>
-            <p>Don't have an account? <Link className='text-blue-500 font-bold underline' to={"/signup"}>Sign up</Link></p>
+            <p>
+              Don't have an account?{" "}
+              <Link
+                className="text-blue-500 font-bold underline"
+                to={"/signup"}
+              >
+                Sign up
+              </Link>
+            </p>
           </form>
         </div>
       </div>
@@ -40,8 +91,7 @@ const Login = () => {
         className="hidden  lg:flex lg:w-1/2 bg-cover bg-right"
         style={{
           backgroundImage: `url(${loginPhoto})`,
-          backgroundPosition:top,
-
+          backgroundPosition: top,
         }}
       ></div>
     </div>
--- src/Pages/auth/Signup.jsx
+++ src/Pages/auth/Signup.jsx
@@ -1,50 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import signupPhoto from "../../assets/signup.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { signUpSchema } from "../../validation/formValidation";
+import axios from "axios";
 
 const Signup = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(signUpSchema),
+  });
+
+  const onSubmit = async (data) => {
+    // Handle form submission, e.g., sending data to an API
+    try{
+      const response = await axios.post("http://localhost:5079/api/User", data);
+      if(response.status === 200){
+        alert("User signup successfull");
+        navigate("/login");
+      }
+      console.log(response);
+    }catch(err){
+      console.log(err);
+    }
+
+  };
+
   return (
     <div className="min-h-screen flex">
       {/* Form Section */}
-      <div className="w-full lg:w-1/2 flex items-center justify-center bg-gray-50 p-8">
+      <div className="w-full lg:w-1/2 flex items-center justify-center bg-gray-50 p-4">
         <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
           <h2 className="text-4xl font-bold text-center mb-10 text-gray-800">
             Sign Up
           </h2>
-          <form className="space-y-8">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
             <div>
               <input
                 className="w-full p-5 border border-blue-400 rounded-lg focus:outline-none focus:border-blue-500"
                 type="text"
+                name="UserName"
                 placeholder="Enter Your Username"
+                {...register("UserName")}
               />
+              {errors.UserName && (
+                <p className="text-red-600">{errors.UserName.message}</p>
+              )}
             </div>
             <div>
               <input
                 className="w-full p-5 border border-blue-400 rounded-lg focus:outline-none focus:border-blue-500"
                 type="email"
+                name="Email"
                 placeholder="Enter Your Email"
+                {...register("Email")}
               />
+              {errors.Email && (
+                <p className="text-red-600">{errors.Email.message}</p>
+              )}
             </div>
             <div>
               <input
                 className="w-full p-5 border border-blue-400 rounded-lg focus:outline-none focus:border-blue-500"
                 type="password"
+                name="Password"
                 placeholder="Enter Your Password"
+                {...register("Password")}
               />
+              {errors.Password && (
+                <p className="text-red-600">{errors.Password.message}</p>
+              )}
             </div>
             <div>
               <input
                 className="w-full p-5 border border-blue-400 rounded-lg focus:outline-none focus:border-blue-500"
                 type="password"
+                name="ConformPassword"
                 placeholder="Confirm Your Password"
+                {...register("ConformPassword")}
               />
+              {errors.ConformPassword && (
+                <p className="text-red-600">{errors.ConformPassword.message}</p>
+              )}
             </div>
-            <div>
-              <select className="w-full p-2 text-center font-bold text-indigo-500" name="role" id="role">
+            <div className="border border-blue-400">
+              <select
+                className="w-full p-2 text-center font-bold text-indigo-500"
+                name="Role"
+                {...register("Role")}
+              >
                 <option value="company">Company</option>
                 <option value="user">User</option>
               </select>
+              {errors.Role && (
+                <p className="text-red-600">{errors.Role.message}</p>
+              )}
+            </div>
+            <div className="flex flex-col items-center gap-[4px] mt-2 justify-center">
+              <div className="flex gap-1">
+                <input type="checkbox" {...register("iAgree")} />
+                <p>
+                  I agree to all{" "}
+                  <span className="text-blue-700">Terms and Conditions</span>
+                </p>
+              </div>
+              {errors.iAgree && (
+                <p className="text-xs lg:text-sm text-red-600 font-semibold">
+                  {errors.iAgree.message}
+                </p>
+              )}
             </div>
             <button
               className="w-full p-5 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-600 transition duration-300"
@@ -52,7 +121,7 @@ const Signup = () => {
             >
               Sign Up
             </button>
-            <p>
+            <p className="text-center">
               Already have an account?{" "}
               <Link className="text-blue-500 font-bold underline" to={"/login"}>
                 Login
